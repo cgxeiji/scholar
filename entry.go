0cfdce88dc3d7e144c3351c3bb315901929e4893
Eiji Onchi <onchi.eiji@gmail.com>
2018-11-01 23:14:19 +0900

    Retun biblatex in alphabetical order. Added documentation.

--- entry.go
+++ entry.go
@@ -7,6 +7,9 @@ import (
 	"time"
 )
 
+// EntryType defines how each entry will be formatted. Each entry has
+// a TYPE of entry, a short DESCRIPTION, REQUIRED fields, and
+// OPTIONAL fields according to BibLaTex documentation.
 type EntryType struct {
 	Type        string
 	Description string            `yaml:"desc"`
@@ -56,6 +59,7 @@ func (e *EntryType) info(level int) {
 	}
 }
 
+// Entry is the basic object of scholar.
 type Entry struct {
 	Type     string            `yaml:"type"`
 	Key      string            `yaml:"key"`
@@ -64,10 +68,13 @@ type Entry struct {
 	File     string            `yaml:"file"`
 }
 
+// Attach attaches a file path to the entry.
 func (e *Entry) Attach(file string) {
 	e.File = file
 }
 
+// Check checks if the fields are formatted correctly.
+// [Currently not useful]
 func (e *Entry) Check() error {
 	date := e.Required["date"]
 	_, err := time.Parse("2006-01-02", date)
@@ -84,14 +91,19 @@ func (e *Entry) Check() error {
 	return nil
 }
 
+// Year returns the year of the entry.
 func (e *Entry) Year() string {
 	return fmt.Sprintf("%.4s", e.Required["date"])
 }
 
+// FirstAuthorLast return the lastname of the first author of the entry.
 func (e *Entry) FirstAuthorLast() string {
 	return strings.Split(e.Required["author"], ",")[0]
 }
 
+// GetKey return the key of the entry. If there is no key, a new key is
+// generated with lastnameYEAR format.
+// For example: einstein1922
 func (e *Entry) GetKey() string {
 	if e.Key == "" {
 		e.Key = fmt.Sprintf("%s%s", strings.ToLower(e.FirstAuthorLast()), e.Year())
@@ -99,18 +111,32 @@ func (e *Entry) GetKey() string {
 	return e.Key
 }
 
+// Bib returns a string with all the information of the entry
+// in BibLaTex format.
 func (e *Entry) Bib() string {
 	bib := fmt.Sprintf("@%s{%s,\n", e.Type, e.GetKey())
-	for field, value := range e.Required {
+	var fields []string
+	for f := range e.Required {
+		fields = append(fields, f)
+	}
+	sort.Strings(fields)
+	for _, field := range fields {
 		if value != "" {
 			bib = fmt.Sprintf("%s  %s = {%s},\n", bib, field, value)
 		}
 	}
-	for field, value := range e.Optional {
+
+	fields = nil
+	for f := range e.Optional {
+		fields = append(fields, f)
+	}
+	sort.Strings(fields)
+	for _, field := range fields {
 		if value != "" {
 			bib = fmt.Sprintf("%s  %s = {%s},\n", bib, field, value)
 		}
 	}
+
 	bib = fmt.Sprintf("%s\n}", bib[:len(bib)-2])
 	return bib
 }
